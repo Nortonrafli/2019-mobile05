e0a32a03d5f74cd74907bf655c94a38b4ed306e5
nortonrafli <norton.nra27@gmail.com>
2019-10-21 15:00:05 +0700

    menyelesaikan tugas fitness

--- app/src/main/java/polinema/ac/id/starterchapter05/fragments/HandstandFragment.java
+++ app/src/main/java/polinema/ac/id/starterchapter05/fragments/HandstandFragment.java
@@ -18,7 +18,7 @@ public class HandstandFragment extends Fragment {
         public View onCreateView (LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState){
             // Inflate the layout for this fragment
-            return inflater.inflate(R.layout.fragment_handstand, container, false);
+            return inflater.inflate(R.layout.fragment_hand_stand,container, false);
         }
 
 }
